0a5649cd7edc165aeb45a77be677a73565159f15
thomphav <thomascarloshogan@gmail.com>
2024-08-05 21:13:22 -0700

    implement pretty table crate

--- src/wc.rs
+++ src/wc.rs
@@ -1,6 +1,23 @@
+use prettytable::{Cell, Row, Table};
 use std::fs::{metadata, File};
 use std::io::{BufRead, BufReader};
 use std::path::Path;
+
+struct TableHeaders {
+    lines: &'static str,
+    bytes: &'static str,
+    words: &'static str,
+    chars: &'static str,
+    file: &'static str,
+}
+
+const HEADERS: TableHeaders = TableHeaders {
+    lines: "Lines",
+    bytes: "Bytes",
+    words: "Words",
+    chars: "Chars",
+    file: "File",
+};
 
 #[derive(Default)]
 struct FlagData {
@@ -34,47 +51,69 @@ pub fn invoke(
     }
 
     if lines || chars || words || default {
-        let b = BufReader::new(File::open(file)?);
+        let reader = BufReader::new(File::open(file)?);
 
         if lines || default {
             data.lines.enabled = true;
-            data.lines.count = b.lines().count();
+            data.lines.count = reader.lines().count();
         }
 
+        let mut reader = BufReader::new(File::open(file)?);
+
+        // for byte in reader.bytes() {
         if chars && !bytes {
             data.chars.enabled = true;
-            data.chars.count = 19 as usize;
+            data.chars.count = 88;
         }
 
         if words || default {
+            let mut buffer = Vec::new();
+            let mut inc = 0;
+            while reader.read_until(b' ', &mut buffer)? != 0 {
+                inc += 1;
+            }
             data.words.enabled = true;
-            data.words.count = 187;
+            data.words.count = inc;
         }
+        // }
     }
 
     assert!(!(data.bytes.enabled & data.chars.enabled));
 
-    let mut buf = String::new();
+    let mut header: Vec<Cell> = Vec::new();
+    let mut values: Vec<Cell> = Vec::new();
+    let mut table = Table::new();
 
     if data.lines.enabled {
-        let out = format!("{} ", data.lines.count);
-        buf.push_str(&out);
+        let out = format!("{}", data.lines.count);
+        header.push(Cell::new(HEADERS.lines));
+        values.push(Cell::new(&out));
     }
-    if data.words.enabled {
-        let out = format!("{} ", data.words.count);
-        buf.push_str(&out);
-    }
-    if data.bytes.enabled {
-        let out = format!("{} ", data.bytes.count);
-        buf.push_str(&out);
-    }
-    if data.chars.enabled {
-        let out = format!("{} ", data.chars.count);
-        buf.push_str(&out);
-    }
-    let out = format!("{}", file.display());
-    buf.push_str(&out);
 
-    println!("{buf}");
+    if data.words.enabled {
+        let out = format!("{}", data.words.count);
+        header.push(Cell::new(HEADERS.words));
+        values.push(Cell::new(&out));
+    }
+
+    if data.bytes.enabled {
+        let out = format!("{}", data.bytes.count);
+        header.push(Cell::new(HEADERS.bytes));
+        values.push(Cell::new(&out));
+    }
+
+    if data.chars.enabled {
+        let out = format!("{}", data.chars.count);
+        header.push(Cell::new(HEADERS.chars));
+        values.push(Cell::new(&out));
+    }
+
+    let fileout = format!("{}", file.display());
+    header.push(Cell::new(HEADERS.file));
+    values.push(Cell::new(&fileout));
+
+    table.add_row(Row::new(header));
+    table.add_row(Row::new(values));
+    table.printstd();
     Ok(())
 }
